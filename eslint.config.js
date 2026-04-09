dee3d44f232ce5eedf1c9cca768eb5d13afb9450
Fela Maslen <felamaslen@gmail.com>
2026-04-09 10:30:20 +0100

    feat: added eslint import order plugin

--- eslint.config.js
+++ eslint.config.js
@@ -1,20 +1,26 @@
 import js from "@eslint/js";
-import tseslint from "typescript-eslint";
 import prettier from "eslint-config-prettier";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import tseslint from "typescript-eslint";
 
 export default tseslint.config(
   js.configs.recommended,
   ...tseslint.configs.recommended,
   prettier,
   {
-    ignores: ["dist/", "**/dist/", "node_modules/"],
+    ignores: ["dist/", "**/dist/", "node_modules/", ".claude/"],
   },
   {
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+    },
     rules: {
       "@typescript-eslint/no-unused-vars": [
         "error",
         { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
       ],
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
     },
   },
 );
